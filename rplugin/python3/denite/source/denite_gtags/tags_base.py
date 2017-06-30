c88969f2e4a1459e6a66da3f75f8ea41ae9cea9c
Oz Elentok <oz.elen@gmail.com>
2017-06-30 10:18:04 +0300

    Fix error on words beginning with '-'

--- rplugin/python3/denite/source/denite_gtags/tags_base.py
+++ rplugin/python3/denite/source/denite_gtags/tags_base.py
@@ -6,21 +6,19 @@ class TagsBase(GtagsBase):
 
     TAG_PATTERN = re.compile('([^\t]+)\t(\\d+)\t(.*)')
 
-    def __init__(self, vim):
-        super().__init__(vim)
-
     @abstractmethod
     def get_search_flags(self):
         return []
 
     def get_search_word(self, context):
-        if len(context['args']) > 0:
+        args_count = len(context['args'])
+        if args_count > 0:
             return context['args'][0]
         return context['input']
 
     def gather_candidates(self, context):
         word = self.get_search_word(context)
-        tags = self.exec_global(self.get_search_flags() + [word], context)
+        tags = self.exec_global(self.get_search_flags() + ['--', word], context)
         candidates = self._convert_to_candidates(tags)
         return candidates
 
